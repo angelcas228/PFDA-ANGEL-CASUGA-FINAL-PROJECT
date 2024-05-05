a8c8c99762baa99cc9df8bb9877fdfa7ebaedb23
Angel Casuga <casugaangela@gmail.com>
2024-05-05 12:31:22 -0500

    fixed def window_size to open image to size of "rat_birthday"

--- src/watermarkplacement.py
+++ src/watermarkplacement.py
@@ -1,14 +1,16 @@
 import pygame
 import os
 
-def window_size(image):
-    win_width = image.get_width()
-    win_height = image.get_height
+pygame.init()
+
+def window_size(image_path):
+    image = pygame.image.load(image_path)
+    win_width, win_height = image.get_width(), image.get_height()
     return win_width, win_height
 
-bg_image_path = "rat_birthday.jpg"
-bg_image = pygame.image.load(bg_image_path).convert_alpha()
 
-win_width, win_height = set_window_size(bg_image)
+bg_image_path = "rat_birthday.jpg"
+win_width, win_height = window_size(bg_image_path)
 win = pygame.display.set_mode((win_width, win_height))
+bg_image = pygame.image.load(bg_image_path).convert_alpha()
 pygame.display.set_caption("Image Overlay")
